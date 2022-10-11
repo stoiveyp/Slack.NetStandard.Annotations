2ff38bf4ae1720a037085d1624abb6465d4b4617
Steven Pears <steven.pears@live.co.uk>
2022-10-11 08:36:12 +0100

    payload example (breaking build)

---
+++ Slack.NetStandard.Annotations.Tests/Examples/InteractionPayload.cs
@@ -0,0 +1,16 @@
+﻿using Slack.NetStandard.Annotations.Markers;
+using Slack.NetStandard.Interaction;
+using Slack.NetStandard.RequestHandler;
+
+namespace Slack.NetStandard.Annotations.Tests.Examples
+{
+    [SlackApp]
+    public partial class Example
+    {
+        [RespondsToPayload("callbackID")]
+        public object ViewSubmissionPayload(ViewSubmissionPayload payload)
+        {
+            return null!;
+        }
+    }
+}
