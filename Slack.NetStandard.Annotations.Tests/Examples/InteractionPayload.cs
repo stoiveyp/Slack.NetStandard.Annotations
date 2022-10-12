50f961042bc168ff3f09e84e4155a56a3dd9229f
Steven Pears <steven.pears@live.co.uk>
2022-10-12 08:31:54 +0100

    Building but failing interaction handler example

--- Slack.NetStandard.Annotations.Tests/Examples/InteractionPayload.cs
+++ Slack.NetStandard.Annotations.Tests/Examples/InteractionPayload.cs
@@ -1,15 +1,38 @@
 ﻿using Slack.NetStandard.Annotations.Markers;
 using Slack.NetStandard.Interaction;
 using Slack.NetStandard.RequestHandler;
+using Slack.NetStandard.RequestHandler.Handlers;
 
 namespace Slack.NetStandard.Annotations.Tests.Examples
 {
     [SlackApp]
     public partial class Example
     {
-        [RespondsToPayload("callbackID")]
+        [RespondsToInteraction("callbackID")]
         public object ViewSubmissionPayload(ViewSubmissionPayload payload)
         {
+            //payload.View.CallbackId
+            return null!;
+        }
+
+        [RespondsToInteraction("actionId")]
+        public object BlockActionPayloadResponse(BlockActionsPayload blocks)
+        {
+            //blocks.Actions[0].ActionId
+            return null!;
+        }
+
+        [RespondsToInteraction("shortcutCallback")]
+        public object ShortcutPayload(ShortcutPayload shortcut)
+        {
+            //shortcut.CallbackId
+            return null!;
+        }
+
+        [RespondsToInteraction("globalShortcutCallback")]
+        public object GlobalPayload(GlobalShortcutPayload globalPayload)
+        {
+            //globalPayload.CallbackId
             return null!;
         }
     }
---
+++ Slack.NetStandard.Annotations.Tests/Snapshots/SnapshotTests.InteractionPayload.verified.cs
@@ -0,0 +1,18 @@
+﻿//HintName: Example.app.g.cs
+using System;
+using Slack.NetStandard;
+using Slack.NetStandard.RequestHandler;
+using Slack.NetStandard.RequestHandler.Handlers;
+using System.Threading.Tasks;
+
+namespace Slack.NetStandard.Annotations.Tests.Examples
+{
+    public partial class Example
+    {
+        private SlackPipeline<object> _pipeline;
+        public void Initialize()
+        {
+            _pipeline = new SlackPipeline<object>(new ISlackRequestHandler<object>[]{new ViewSubmissionPayloadHandler(this), new BlockActionsPayloadResponseHandler(this), new ShortcutPayloadHandler(this), new GlobalPayloadHandler(this)});
+        }
+    }
+}
